b8dd7e86f429962d2ce8d2771161ea979e4a0c09
tmax818 <tylermaxwell661@gmail.com>
2022-09-26 21:04:47 -0700

    init commit

---
+++ src/Order.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+// Here we're creating a new data type called Order
+public class Order {
+
+    // MEMBER VARIABLES
+    private String name; // default value of null
+    private boolean ready; // default value false
+    private ArrayList<Item> items; // defaults to null
+
+    // CONSTRUCTOR
+    // No arguments, sets name to "Guest", initializes items as an empty list.
+
+    // OVERLOADED CONSTRUCTOR
+    // Takes a name as an argument, sets name to this custom name.
+    // Initializes items as an empty list.
+
+    // ORDER METHODS
+
+    // Most of your code will go here,
+    // so implement the getters and setters first!
+
+    // GETTERS & SETTERS
+
+}
+
