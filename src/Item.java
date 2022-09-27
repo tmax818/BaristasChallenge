b611a586130a50531ca8b794706910b9b1ec708d
tmax818 <tylermaxwell661@gmail.com>
2022-09-26 21:06:46 -0700

    update item

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,13 @@
 public class Item {
+
+    // MEMBER VARIABLES
+    private String name;
+    private double price;
+
+    // CONSTRUCTOR
+    //   Takes a name and price as arguments
+    //   and sets them accordingly
+
+    // GETTERS & SETTERS  - for name and price
+
 }
