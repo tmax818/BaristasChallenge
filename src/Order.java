a307079a5e59b631ecbeef7295bc9525bbef9bc0
tmax818 <tylermaxwell661@gmail.com>
2022-09-26 21:38:37 -0700

    fin sol

--- src/Order.java
+++ src/Order.java
@@ -6,21 +6,81 @@ public class Order {
     // MEMBER VARIABLES
     private String name; // default value of null
     private boolean ready; // default value false
-    private ArrayList<Item> items; // defaults to null
+    private ArrayList<Item> items = new ArrayList<>(); // defaults to null
 
     // CONSTRUCTOR
     // No arguments, sets name to "Guest", initializes items as an empty list.
+    public Order() {
+        this.name = "Guest";
+        this.ready = false;
+    }
 
     // OVERLOADED CONSTRUCTOR
     // Takes a name as an argument, sets name to this custom name.
     // Initializes items as an empty list.
+
+    public Order(String name) {
+        this.name = name;
+    }
+
 
     // ORDER METHODS
 
     // Most of your code will go here,
     // so implement the getters and setters first!
 
+    public void addItem(Item item){
+        this.items.add(item);
+    }
+
+    public String getStatusMessage(){
+        if(this.ready){
+            return "Your order is ready.";
+        } else {
+            return "Thank you for waiting. Your order will be ready soon.";
+        }
+    }
+
+    public double getOrderTotal(){
+        double total = 0d;
+        for(Item item: this.items){
+            total += item.getPrice();
+        }
+        return total;
+    }
+
+    public void display(){
+        System.out.format("Customer Name: %s\n", this.name);
+        for(Item item: this.items){
+            System.out.format("%s - $%,.2f\n", item.getName(), item.getPrice());
+        }
+        System.out.format("Total: $%,.2f\n", this.getOrderTotal());
+    }
+
     // GETTERS & SETTERS
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isReady() {
+        return ready;
+    }
+
+    public void setReady(boolean ready) {
+        this.ready = ready;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(ArrayList<Item> items) {
+        this.items = items;
+    }
 }
 
